e8f38158b6f76c39a0725bb41d85a2d305ae8d62
haru-1chi <jarunee.recc2@gmail.com>
2023-11-17 11:19:25 +0700

    first commit

---
+++ EX4.php
@@ -0,0 +1,17 @@
+<?php
+$num = 3;
+switch ($num) {
+    case '1':
+        echo 'Best';
+        break;
+    case '2':
+        echo 'Good';
+        break;
+    case '3':
+        echo 'Normal';
+        break;
+    default:
+        echo 'Dont Know';
+        break;
+}
+?>
