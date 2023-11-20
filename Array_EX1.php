ee98b7e867bd8804e2917388d1a79a92e036ccfe
haru-1chi <jarunee.recc2@gmail.com>
2023-11-20 10:42:08 +0700

    first commit

--- Array_EX1.php
+++ Array_EX1.php
@@ -1,21 +1,18 @@
 <?php
     $nums = array(6,7,3,1,2,5,10,4,8,9);
-    $sum = 0;
-    echo "array = [";
+    echo "Array = [";
     for($i=0;$i<count($nums);$i++){
         if($i==(count($nums)-1)){
             echo $nums[$i];
         }else{
             echo $nums[$i].", ";
         }
-        $sum = $sum+$nums[$i];
     }
     // สำหรับ foreach
     // foreach ($nums as $num) {
     //     echo $num." ";
-    //     $sum = $sum+$num;
     // }
-    echo "]<br>"."Sum = ".$sum;
+    echo "]<br>"."Sum = ".array_sum($nums);
     echo "<br>"."Max in array =".max($nums);
     echo "<br>"."Min in array =".min($nums);
 ?>
---
+++ Array_EX2.php
@@ -0,0 +1,13 @@
+<?php
+$ingredients = array(
+    'อัลมอนด์ป่น' => 50,
+    'น้ำตาลไอซิ่ง' => 40,
+    'น้ำตาลทราย' => 30,
+    'ไข่ขาว' => 35,
+    'โกโก้' => 10
+);
+    array_push($a,'น้ำตาลทราย' => 30);
+    foreach ($ingredients as $key => $value) {
+        echo $key." ".$value." กรัม"."<br>";
+    }
+?>
