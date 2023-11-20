c24dcaac51df4ae4ecf62e2b291012c594562e13
haru-1chi <jarunee.recc2@gmail.com>
2023-11-20 15:05:53 +0700

    first commit

--- Array_EX5.php
+++ Array_EX5.php
@@ -1,3 +1,33 @@
 <?php
-    $items = [23, "apple", 42, "42", 23, "banana", 5, "apple", 5.5, null, true, 42];    
+$items = [23, "apple", 42, "42", 23, "banana", 5, "apple", 5.5, null, true, 42];
+echo "items = ";
+foreach ($items as $key) {
+    echo $key . " ";
+}
+echo "<br>";
+$int_item = array();
+echo "<br>กรองออกทุกค่าที่ไม่ใช่ตัวเลขจำนวนเต็ม<br>";
+foreach ($items as $key) {
+    if (is_int($key)) {
+        array_push($int_item, $key);
+    }
+}
+foreach ($int_item as $key) {
+    echo $key . " ";
+}
+echo "<br><br>สร้างอาร์เรย์ใหม่ที่มีเฉพาะค่าที่ไม่ซ้ำกัน<br>";
+$new_array = array();
+
+$new_array = array_unique($items);
+
+foreach ($new_array as $key) {
+    echo $key . " ";
+}
+
+echo "<br><br> กลับลำดับขององค์ประกอบในอาร์เรย์<br>";
+$rev_array = array_reverse($items);
+foreach ($rev_array as $key) {
+    echo $key . " ";
+}
+
 ?>
--- Array_EX6.php
+++ Array_EX6.php
@@ -1,4 +1,25 @@
 <?php
-    $array1 = [10, 20, 30, 40, 50];
-    $array2 = [60, 70, 80, 90, 100];    
+$array1 = [10, 20, 30, 40, 50];
+$array2 = [60, 70, 80, 90, 100];
+$new_array = array_merge($array1, $array2);
+echo "new_array = ";
+foreach ($new_array as $key) {
+    echo $key . " ";
+}
+echo "<br>ตัด 4 ตัวแรก = ";
+$new_array1 = array_slice($new_array, 4);
+foreach ($new_array1 as $key) {
+    echo $key . " ";
+}
+echo "<br>ตัด 3 ตัวท้าย = ";
+$new_array2 = array_slice($new_array, 4, 3);
+foreach ($new_array2 as $key) {
+    echo $key . " ";
+}
+echo "<br>";
+if (in_array(50, $new_array2)) {
+    echo "มีตัวเลขนี้ใน array";
+} else {
+    echo "ไม่มีตัวเลขนี้ใน array";
+}
 ?>
