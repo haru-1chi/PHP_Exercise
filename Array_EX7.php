ed7b8338563e323b426e97d12b9f060413fd4c15
haru-1chi <jarunee.recc2@gmail.com>
2023-11-20 15:35:28 +0700

    first commit

--- Array_EX7.php
+++ Array_EX7.php
@@ -1,4 +1,17 @@
 <?php
-    $array = ['computer', 'juice', 'telephone', 'floor', 'table', 'umbrella'];
-    echo count($fruits);
+$array = ['computer', 'juice', 'telephone', 'floor', 'table', 'umbrella'];
+if (in_array('telephone', $array)) {
+    echo "มี telephone ใน array";
+} else {
+    echo "ไม่มี telephone ใน array";
+}
+echo "<br>";
+$index = array_search("table", $array);
+if ($index !== false) {
+    $array = array_replace($array, [$index => "Car"]);
+}
+
+foreach ($array as $Num) {
+    echo $Num . " ";
+}
 ?>
--- Array_EX8.php
+++ Array_EX8.php
@@ -1,4 +1,16 @@
 <?php
-    $fruits = ["Apple", "Banana", "Orange", "Grape", "Mango", "Berry"];
-    echo count($fruits);
+$fruits = ["Apple", "Banana", "Orange", "Grape", "Mango", "Berry"];
+echo "จำนวนองค์ประกอบ " . count($fruits)."<br>สับเปลี่ยนองค์ประกอบอย่างสุ่ม ";;
+shuffle($fruits);
+foreach ($fruits as $Num) {
+    echo $Num . " ";
+}
+echo "<br>";
+
+unset($fruits[5]);
+
+foreach ($fruits as $key => $value) {
+    echo "[".$key . "] => ".$value."<br>";
+}
+
 ?>
